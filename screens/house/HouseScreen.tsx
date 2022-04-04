4b38d177295dce8f47c3f1309ce8cb005ed419bb
Ntholi Nkhatho <ntholi@live.com>
2022-04-04 11:10:52 +0200

    Add LinearGradient

--- screens/house/HouseScreen.tsx
+++ screens/house/HouseScreen.tsx
@@ -1,21 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import colors from '../utils/colors';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function HouseScreen({ route, navigation }: any) {
   const { name } = route.params.house;
   return (
-    <View style={styles.container}>
-      <Text style={styles.houseName}>{name}</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={'#43A047'} />
+      <View style={styles.container}>
+        <LinearGradient
+          colors={['#66BB6A', colors.dark]}
+          style={styles.linearGradient}
+        >
+          <Text style={styles.houseName}>{name}</Text>
+        </LinearGradient>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignContent: 'center',
     backgroundColor: colors.dark,
+  },
+  linearGradient: {
+    height: '30%',
+    justifyContent: 'flex-end',
+    paddingLeft: 10,
   },
   houseName: {
     fontSize: 30,
