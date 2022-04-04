f64dc6b511d4ef6c8fedd0740d0b97a13c076588
Ntholi Nkhatho <ntholi@live.com>
2022-04-04 10:49:20 +0200

    Make HouseScreen text visible

--- screens/house/HouseScreen.tsx
+++ screens/house/HouseScreen.tsx
@@ -3,21 +3,22 @@ import React from 'react';
 import colors from '../utils/colors';
 
 export default function HouseScreen({ route, navigation }: any) {
-  const { name } = route.params;
+  const { name } = route.params.house;
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.houseName}>{name}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  houseName: {
-    fontSize: 30,
-  },
   container: {
     flex: 1,
     alignContent: 'center',
     backgroundColor: colors.dark,
+  },
+  houseName: {
+    fontSize: 30,
+    color: colors.light,
   },
 });
