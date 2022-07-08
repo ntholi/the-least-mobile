094402682d5cc72252729912fcb6f78a58ebb93e
Ntholi Nkhatho <ntholi@live.com>
2022-07-08 15:11:04 +0200

    Clean reset

--- components/house/house.ts
+++
@@ -1,9 +0,0 @@
-export interface House {
-  id: string;
-  name: string;
-  address: string; // village
-  region: string; // district
-  country: string;
-  target: number;
-  donated: number;
-}
--- components/user/user.ts
+++
@@ -1,6 +0,0 @@
-export default interface User {
-  id: string;
-  names: string;
-  email: string;
-  password: string;
-}
