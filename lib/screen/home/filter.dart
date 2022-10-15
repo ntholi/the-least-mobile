4742004645516a0deb0a329e214a2f7081e84fc9
Ntholi Nkhatho <ntholi@live.com>
2022-10-15 12:36:08 +0200

    Add icon filters

--- lib/screen/home/filter.dart
+++ lib/screen/home/filter.dart
@@ -10,40 +10,61 @@ class Filter extends StatefulWidget {
 }
 
 class _FilterState extends State<Filter> {
-  final items = ["Near You", "Favorites", "Something Else"];
+  final items = ["Near You", "Favorites", "New"];
+  final icons = ["address", "star", "new"];
   var selected = "";
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+      padding: const EdgeInsets.only(top: 20),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade50,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
       ),
-      height: 80,
+      height: 150,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         padding: const EdgeInsets.all(8),
         itemCount: items.length,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            margin: const EdgeInsets.only(right: 10),
-            child: ChoiceChip(
-              side: const BorderSide(color: Colors.grey),
-              padding: const EdgeInsets.all(13),
-              backgroundColor: Colors.white,
-              label: Text(items[index]),
-              labelStyle: TextStyle(
-                color: items[index] == selected ? Colors.white : Colors.black,
+          return InkWell(
+            child: Container(
+              width: 100,
+              margin: const EdgeInsets.only(right: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 1,
+                    offset: const Offset(0, 1),
+                  ),
+                ],
               ),
-              selected: items[index] == selected,
-              selectedColor: Colors.black87,
-              onSelected: (bool value) {
-                setState(() {
-                  selected = value ? items[index] : "";
-                });
-              },
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: Image.asset('assets/images/${icons[index]}.png'),
+                  ),
+                  Text(
+                    items[index],
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey.shade500,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )
+                ],
+              ),
             ),
           );
         },
