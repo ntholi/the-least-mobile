d4595a0da2c6aef89b6ac1c1d8e90c082678b30e
Ntholi Nkhatho <ntholi@live.com>
2022-11-07 10:54:56 +0200

    Add image to AddMpesaPage widget

--- lib/screen/payment/methods/add_mpesa.dart
+++ lib/screen/payment/methods/add_mpesa.dart
@@ -17,96 +17,75 @@ class AddMpesaPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Stack(
-            children: [
-              SizedBox(
-                height: 140,
-                child: AppBar(
-                  title: const Text("M-Pesa"),
-                  elevation: 0,
+      appBar: AppBar(
+        title: const Text("M-Pesa"),
+        elevation: 0,
+      ),
+      body: ColoredBox(
+        color: AppColors.primaryColor,
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.grey.shade50,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(50),
+              topRight: Radius.circular(50),
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: Image.asset("assets/images/digital-wallet.png"),
                 ),
-              ),
-              Container(
-                height: 70,
-                margin: const EdgeInsets.only(top: 70),
-                child: const Center(
-                  child: SizedBox(
-                    // child: Image.asset('assets/images/mpesa.png'),
-                    child: Icon(
-                      Icons.phone_android,
-                      color: Colors.white,
+                Text(
+                  "Add M-Pesa Number",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey.shade500,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Please enter the your M-Pesa Phone number to process payment",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 30),
+                Form(
+                  key: formKey,
+                  child: TextFormField(
+                    onSaved: (value) => _phoneNumber = value ?? "",
+                    decoration: const InputDecoration(
+                      labelText: "Phone Number",
+                      border: OutlineInputBorder(),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Expanded(
-            child: ColoredBox(
-              color: AppColors.primaryColor,
-              child: Container(
-                padding: const EdgeInsets.only(top: 80, left: 30, right: 30),
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade50,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50),
-                  ),
-                ),
-                child: SingleChildScrollView(
-                  child: Form(
-                    key: formKey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          onSaved: (value) => _phoneNumber = value ?? "",
-                          decoration: const InputDecoration(
-                            labelText: "Phone Number",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        TextFormField(
-                          onSaved: (value) => _password = value ?? "",
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            labelText: "Password",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 50),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+                const SizedBox(height: 40),
+                Button(
+                  title: "Save",
+                  onClick: () {
+                    formKey.currentState?.save();
+                    Map<String, dynamic> fields = {
+                      "password": _password,
+                    };
+                    final method = PaymentMethod(
+                      id: _phoneNumber,
+                      type: PaymentType.mpesa,
+                      fields: fields,
+                    );
+                    addPaymentMethod(method, user);
+                  },
+                )
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 30,
-              vertical: 16,
-            ),
-            child: Button(
-              title: "Save",
-              onClick: () {
-                formKey.currentState?.save();
-                Map<String, dynamic> fields = {
-                  "password": _password,
-                };
-                final method = PaymentMethod(
-                  id: _phoneNumber,
-                  type: PaymentType.mpesa,
-                  fields: fields,
-                );
-                addPaymentMethod(method, user);
-              },
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
